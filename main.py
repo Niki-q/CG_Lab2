3e27820749a27651bb7f8d9db2e2dc72e8145218
Nikita <petrychenko.nikita.learn@gmail.com>
2021-11-10 20:16:01 +0200

    Lab2 Computer Graphics Petrychenko KM-01

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from PIL import ImageDraw
 
 width = 540
 height = 960
-
+# Встановлює розміри вікна (полотна – canvas size) 540х960 пкс;
 image = Image.new("RGB", (width, height))
 
 draw = ImageDraw.Draw(image)
 
-f = open('DS9.txt')
+f = open('DS3.txt')
+# Зчитує датасет з файлу;
 for line in f:
     point_cord = line.split(" ")
     x = int(point_cord[0])
     y = int(point_cord[1])
     draw.point((x, y), fill=ImageColor.getrgb("red"))
+    # Відображає точки за заданими координатами
 
 image.save("empty.png", "PNG")
+# Виводить результат в будь-який графічний формат.
